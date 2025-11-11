7852c69f041202b0800878bd46adc0f283630f3d
Brooklyn Zhou <brooklyn.zhou@mail.utoronto.ca>
2025-11-10 22:28:30 -0500

    User class

--- src/main/java/Entities/User.java
+++ src/main/java/Entities/User.java
@@ -11,6 +11,9 @@ public class User {
     private ArrayList<Comments> myComments =   new ArrayList<>();
 
     public User(String userName, String password) {
+        if (userName == null || password == null) {
+            throw new IllegalArgumentException("Username or password cannot be empty");
+        }
         this.username = userName;
         this.password = password;
     }
@@ -23,12 +26,19 @@ public class User {
         return password;
     }
 
-    public void resetPassword(String newPassword) {
+    public void changePassword(String newPassword) {
         this.password = newPassword;
     }
 
     public void addMyListing(Listing listing){
+        if(!myListings.contains(listing)){
+            throw new IllegalArgumentException("Listing not found");
+        }
         myListings.add(listing);
+    }
+
+    public void removeMyListing(Listing listing){
+        myListings.remove(listing);
     }
 
     public ArrayList<Listing> getMyListings() {
@@ -39,12 +49,26 @@ public class User {
         favourite.add(listing);
     }
 
+    public void removeFavourite(Listing listing){
+        if(!favourite.contains(listing)){
+            throw new IllegalArgumentException("Listing not found");
+        }
+        myListings.remove(listing);
+    }
+
     public ArrayList<Listing> getFavourite() {
         return favourite;
     }
 
     public void addComment(Comments comment){
         myComments.add(comment);
+    }
+
+    public void removeComment(Comments comment){
+        if(!myComments.contains(comment)){
+            throw new IllegalArgumentException("Comment not found");
+        }
+        myComments.remove(comment);
     }
 
     public ArrayList<Comments> getMyComments() {
