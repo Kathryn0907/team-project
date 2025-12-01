8336123d320384b4cd1a7eaf237588a883f12adb
Brooklyn Zhou <brooklynzhou249@gmail.com>
2025-11-30 22:36:33 -0500

    Back button in listing detail.

--- src/main/java/view/ListingDetailView.java
+++ src/main/java/view/ListingDetailView.java
@@ -56,9 +56,7 @@ public class ListingDetailView extends JPanel implements PropertyChangeListener 
         JButton backButton = new JButton("← Back");
         backButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         backButton.addActionListener(e -> {
-            LoggedInState loggedInState = new LoggedInState();
-            loggedInState.setUser(viewModel.getState().getCurrentUser());
-            viewManagerModel.setState("logged in");
+            viewManagerModel.setState("search");
             viewManagerModel.firePropertyChange();
         });
         headerPanel.add(backButton, BorderLayout.WEST);
